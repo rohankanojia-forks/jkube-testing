67da62e7003519e552d36afd81bccff07fa4a4ef
Rohan Kumar <rohaan@redhat.com>
2023-06-14 10:43:09 +0530

    chore : Add reproducer for sidecar with openshift maven plugin
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

---
+++ jkube-service-fragment-demo/src/main/java/org/eclipse/jkube/testing/JkubeServiceFragmentDemoApplication.java
@@ -0,0 +1,13 @@
+package org.eclipse.jkube.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkubeServiceFragmentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkubeServiceFragmentDemoApplication.class, args);
+	}
+
+}
